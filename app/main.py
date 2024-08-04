8ff282f3008c0e1e83976a47492df84f99eb6839
AbhijithSwaminathan <abhiachu2@gmail.com>
2024-08-04 23:43:00 +0530

    Added database and its usage

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+
+from .database import database, engine, metadata, redis
+
 
 app = FastAPI()
+
+metadata.create_all(engine)
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+    await redis.ping()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+    await redis.close()
 
 @app.get("/")
 def read_root():
